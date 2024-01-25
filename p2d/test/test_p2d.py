c086d73f025036823ee0c15419347a68d19bad64
Dup4 <lyuzhi.pan@gmail.com>
2024-01-25 23:06:25 +0800

    refactor: test dir name
    Signed-off-by: Dup4 <lyuzhi.pan@gmail.com>

--- p2d/test/test_p2d.py
+++ p2d/test/test_p2d.py
@@ -18,16 +18,16 @@ def test_p2d():
         shutil.rmtree(test_output_dir)
     os.mkdir(test_output_dir)
 
-    package_zip_dir = test_data_dir / "little-h-reboot-7$linux.zip"
-    target_package_zip_dir = test_output_dir / "example.zip"
-    package_dir = test_output_dir / "example"
-    shutil.copyfile(package_zip_dir, target_package_zip_dir)
-    with zipfile.ZipFile(target_package_zip_dir, 'r') as zip_ref:
+    polygon_package_zip_dir = test_data_dir / "little-h-reboot-7$linux.zip"
+    target_polygon_package_zip_dir = test_output_dir / "example-polygon.zip"
+    package_dir = test_output_dir / "example-polygon"
+    shutil.copyfile(polygon_package_zip_dir, target_polygon_package_zip_dir)
+    with zipfile.ZipFile(target_polygon_package_zip_dir, 'r') as zip_ref:
         zip_ref.extractall(package_dir)
 
-    output_file = test_output_dir / "example"
+    output_file = test_output_dir / "example-domjudge"
     with tempfile.TemporaryDirectory(prefix="p2d-domjudge-test") as temp_dir:
         p = Polygon2DOMjudge(package_dir, temp_dir, output_file)
         p.process()
 
-    assert (test_output_dir / "example.zip").exists()
+    assert (test_output_dir / "example-domjudge.zip").exists()
