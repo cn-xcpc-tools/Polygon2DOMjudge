0e85c7ed2a441bd758e1e00df80a2d1cac1742ea
cubercsl <2014cais01@gmail.com>
2024-01-26 21:45:30 +0800

    fix: release script

--- release.sh
+++ release.sh
@@ -1,6 +1,4 @@
-#! /bin/bash
-
-CUR_DIR="$(dirname "$(realpath "${BASH_SOURCE[0]}")")"
+#! /bin/bash -e
 
 VERSION="${1}"
 if [ -z "${VERSION}" ]; then
@@ -10,13 +8,9 @@ fi
 
 poetry version "${VERSION}"
 
-pushd "${CUR_DIR}/.." || exit 1
-
 git add .
 git commit -m "chore: release v${VERSION}"
 git tag v"${VERSION}"
 git push --tags
 
 echo "Release v${VERSION} created."
-
-popd || exit 1
